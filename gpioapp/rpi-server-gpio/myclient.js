41674de4024ed3da42b360eee923407d25ca33e6
Juan Greco <jgrecoarroyo@gmail.com>
2015-09-22 11:07:48 +0200

    pinout corrected

--- gpioapp/rpi-server-gpio/myclient.js
+++ gpioapp/rpi-server-gpio/myclient.js
@@ -10,8 +10,7 @@
 window.onload = function () {
   var url,
     i,
-    ports = [7, 11, 12, 13];  // the GPIO ports we will read
-    //ports = [7, 11,12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 35, 36, 37, 38, 40];  // the GPIO ports we will read
+    ports = [7, 11,12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 35, 36, 37, 38, 40];  // the GPIO ports we will read
 
   for (i in ports) {
     $('#input_' + ports[i]).html('loading port ' + ports[i] + ' value...');
@@ -23,8 +22,8 @@ window.onload = function () {
       console.log('making API call ' + url);
 
       $.getJSON(url, function (data) {
-        console.log('API response received. port ' + data.gpio + ' value = ' + data.value);
-        $('#input_' + data.gpio).html('GPIO input port ' + data.gpio + ' value is ' + data.value);
+        console.log('API response received. port ' + data.pin + ' value = ' + data.value);
+        $('#input_' + data.pin).html('PIN input port ' + data.pin + ' value is ' + data.value);
       });
     } // for
   }, 1000); // setInterval
