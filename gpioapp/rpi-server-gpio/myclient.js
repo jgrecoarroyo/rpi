14d26f6b29b62dacb95c377387e4e2383648658c
Juan Greco <jgrecoarroyo@gmail.com>
2015-09-22 10:50:27 +0200

    mods in the main page

--- gpioapp/rpi-server-gpio/myclient.js
+++ gpioapp/rpi-server-gpio/myclient.js
@@ -1,16 +1,16 @@
 /**
  * MYCLIENT.JS
- * an example web app using an ajax request to our API server which returns a JSON object 
- * 
- * When a user opens index.html it then loads and executes this JavaScript code, which reads 
- * the current logic level on the input ports, and displays that in the window, each in a div 
+ * an example web app using an ajax request to our API server which returns a JSON object
+ *
+ * When a user opens index.html it then loads and executes this JavaScript code, which reads
+ * the current logic level on the input ports, and displays that in the window, each in a div
  * identified by the index "inputs_[port number]"
  */
 
 window.onload = function () {
   var url,
     i,
-    ports = [23, 25];  // the GPIO ports we will read
+    ports = [7, 11,12, 13, 15, 61, 18, 22, 29, 31, 32, 33, 35, 36, 37, 38, 40];  // the GPIO ports we will read
 
   for (i in ports) {
     $('#input_' + ports[i]).html('loading port ' + ports[i] + ' value...');
@@ -18,18 +18,14 @@ window.onload = function () {
 
   setInterval( function () {
     for (i in ports) {
-      url = document.URL + 'inputs/' + ports[i];
+      url = document.URL + 'pins/' + ports[i];
       console.log('making API call ' + url);
 
       $.getJSON(url, function (data) {
         console.log('API response received. port ' + data.gpio + ' value = ' + data.value);
         $('#input_' + data.gpio).html('GPIO input port ' + data.gpio + ' value is ' + data.value);
       });
-    } // for 
+    } // for
   }, 1000); // setInterval
-  
+
 }; //onload
-
-
-
-
